835dcf50eb6b4ed6f928b1dbff010ad2453024db
Sebastian Freundt <freundt@ga-group.nl>
2013-01-09 10:08:24 +0000

    revert, don't reuse old cells on the subq
    The problem is that there's a pointer associated with it, so we'd introduce memleaks if we
    just copied the struct sub_s object over the old one.
    
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/sub.c
+++ src/sub.c
@@ -146,19 +146,12 @@ void
 subq_add(subq_t sq, struct sub_s s)
 {
 	/* get us a free item */
-	sub_qqq_t si;
-	int need_push_p = 0;
+	sub_qqq_t si = pop_q(sq);
 
-	if ((si = find_cell(sq->sbuf, s.idx)) == NULL) {
-		si = pop_q(sq);
-		need_push_p = 1;
-	}
 	/* just copy the whole shebang */
 	si->s = s;
-	/* and maybe push it */
-	if (UNLIKELY(need_push_p)) {
-		gq_push_tail(sq->sbuf, (gq_item_t)si);
-	}
+	/* and push it */
+	gq_push_tail(sq->sbuf, (gq_item_t)si);
 	SUB_DEBUG("PUSH SQ %p\n", si);
 	return;
 }
