1fe84e0e4a55546d4d7ae7ad8c0f42d6e99212c9
Sebastian Freundt <freundt@ga-group.nl>
2013-01-10 10:04:29 +0000

    minor, use EV_CUSTOM to propagate hangups in twsc_cb() and EV_CLEANUP also along with NULL object to notify reco_cb()
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/quo-tws.c
+++ src/quo-tws.c
@@ -630,13 +630,14 @@ clo:
 }
 
 static void
-twsc_cb(EV_P_ ev_io w[static 1], int UNUSED(rev))
+twsc_cb(EV_P_ ev_io w[static 1], int rev)
 {
 	static char noop[1];
 	ctx_t ctx = w->data;
 
 	QUO_DEBUG("BANG  %x\n", rev);
-	if (recv(w->fd, noop, sizeof(noop), MSG_PEEK) <= 0) {
+	if ((rev & EV_CUSTOM)/*hangup requested*/ ||
+	    recv(w->fd, noop, sizeof(noop), MSG_PEEK) <= 0) {
 		/* perform some clean up work on our data */
 		twsc_conn_clos(ctx);
 		/* uh oh */
@@ -663,11 +664,9 @@ reco_cb(EV_P_ ev_timer *w, int UNUSED(revents))
 	/* going down? */
 	if (UNLIKELY(w == NULL)) {
 		QUO_DEBUG("FINI  %d\n", twsc->fd);
-		/* close tws client's socket (gracefully) */
-		shut_sock(twsc->fd);
 		/* call that twsc watcher manually to free subq resources
 		 * mainloop isn't running any more */
-		twsc_cb(EV_A_ twsc, 0);
+		twsc_cb(EV_A_ twsc, EV_CUSTOM);
 		return;
 	}
 	/* otherwise proceed normally */
@@ -978,7 +977,7 @@ main(int argc, char *argv[])
 	ev_prepare_stop(EV_A_ prep);
 
 	/* propagate tws shutdown and resource freeing */
-	reco_cb(EV_A_ NULL, 0);
+	reco_cb(EV_A_ NULL, EV_CUSTOM | EV_CLEANUP);
 
 	/* finalise quote queue */
 	free_quoq(ctx->qq);
