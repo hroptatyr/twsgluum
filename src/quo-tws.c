3cc9b4ce671b6916f3ecc1eb5e405ee42109f1ef
Sebastian Freundt <freundt@ga-group.nl>
2013-01-10 11:38:55 +0000

    minor, allow to specify log file for quo-tws
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/quo-tws.c
+++ src/quo-tws.c
@@ -881,9 +881,10 @@ main(int argc, char *argv[])
 		perror("daemonisation failed");
 		res = 1;
 		goto out;
-	} else if (argi->daemonise_given) {
-		/* logging */
-		open_logerr("/tmp/quo-tws.log");
+	} else if (argi->log_given && open_logerr(argi->log_arg) < 0) {
+		perror("cannot open log file");
+		res = 1;
+		goto out;
 	}
 
 	/* initialise the main loop */
