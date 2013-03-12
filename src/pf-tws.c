437e2a64cd807f29589a0369638571e06d7363a9
Sebastian Freundt <freundt@ga-group.nl>
2013-03-12 11:44:49 +0000

    minor, replay ini-connect goodies against pf-tws
    Signed-off-by: Sebastian Freundt <freundt@ga-group.nl>

--- src/pf-tws.c
+++ src/pf-tws.c
@@ -488,8 +488,19 @@ reco_cb(EV_P_ ev_timer *w, int UNUSED(revents))
 		ev_io_shut(EV_A_ twsc);
 		return;
 	}
-	/* otherwise proceed normally */
-	ctx = w->data;
+
+	switch ((ctx = w->data, tws_state(ctx->tws))) {
+	case TWS_ST_SUP:
+	case TWS_ST_RDY:
+	default:
+		/* connection is up and running */
+		return;
+	case TWS_ST_UNK:
+	case TWS_ST_DWN:
+		break;
+	}
+
+	/* otherwise proceed with the (re)connect */
 	if ((s = make_tws_sock(ctx->uri)) < 0) {
 		error(errno, "tws connection setup failed");
 		return;
@@ -508,9 +519,6 @@ reco_cb(EV_P_ ev_timer *w, int UNUSED(revents))
 		ev_io_shut(EV_A_ twsc);
 		return;
 	}
-	/* and lastly, stop ourselves */
-	ev_timer_stop(EV_A_ w);
-	w->data = NULL;
 	return;
 }
 
@@ -528,16 +536,15 @@ prep_cb(EV_P_ ev_prepare *w, int UNUSED(revents))
 	PF_DEBUG("STAT  %u\n", st);
 	switch (st) {
 	case TWS_ST_UNK:
-	case TWS_ST_DWN:
-		/* is there a timer already? */
 		if (reco->data == NULL) {
-			/* start the reconnection timer */
-			reco->data = ctx;
 			ev_timer_init(reco, reco_cb, 0.0, 2.0/*option?*/);
 			ev_timer_start(EV_A_ reco);
-			PF_DEBUG("RECO\n");
+			reco->data = ctx;
 		}
+	case TWS_ST_DWN:
+		/* let the reco timer do the work */
 		break;
+
 	case TWS_ST_RDY:
 		if (old_st != TWS_ST_RDY) {
 			PF_DEBUG("SUBS\n");
